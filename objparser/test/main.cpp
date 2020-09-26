b8c11d8d0858dc7e521902c2d6d7a303b187f949
Danila Kryukov <danila.kryukov.dev@yandex.ru>
2020-09-26 22:53:50 +0700

    add detailed test for cube
    Signed-off-by: Danila Kryukov <danila.kryukov.dev@yandex.ru>

--- objparser/test/main.cpp
+++ objparser/test/main.cpp
@@ -69,6 +69,70 @@ TEST_CASE("checkCube"){
     CHECK(test.getFormat() ==  'a');
     CHECK(test.getVertices().size() == test.getTextures().size());
     CHECK(test.getVertices().size() == test.getNormals().size());
+
+    SUBCASE("detailedCheckCube1"){
+        QVector3D fiveV(-1,1,-1);
+        REQUIRE(test.getVertices()[0].x() == fiveV.x());
+        REQUIRE(test.getVertices()[0].y() == fiveV.y());
+        REQUIRE(test.getVertices()[0].z() == fiveV.z());
+
+        QVector3D fiveN(0,1,0);
+        CHECK(test.getNormals()[0].x() == fiveN.x());
+        CHECK(test.getNormals()[0].y() == fiveN.y());
+        CHECK(test.getNormals()[0].z() == fiveN.z());
+
+        QVector2D fiveT(0.875000, 0.500000);
+        CHECK(test.getTextures()[0].x() == fiveT.x());
+        CHECK(test.getTextures()[0].y() == fiveT.y());
+    }
+
+    SUBCASE("detailedCheckCube2"){
+        QVector3D fiveV(1,1,-1);
+        CHECK(test.getVertices()[2].x() == fiveV.x());
+        CHECK(test.getVertices()[2].y() == fiveV.y());
+        CHECK(test.getVertices()[2].z() == fiveV.z());
+
+        QVector3D fiveN(0,1,0);
+        CHECK(test.getNormals()[2].x() == fiveN.x());
+        CHECK(test.getNormals()[2].y() == fiveN.y());
+        CHECK(test.getNormals()[2].z() == fiveN.z());
+
+        QVector2D fiveT(0.625000, 0.500000);
+        CHECK(test.getTextures()[2].x() == fiveT.x());
+        CHECK(test.getTextures()[2].y() == fiveT.y());
+    }
+
+    SUBCASE("detailedCheckCube3"){
+        QVector3D fiveV(1,1,1);
+        CHECK(test.getVertices()[3].x() == fiveV.x());
+        CHECK(test.getVertices()[3].y() == fiveV.y());
+        CHECK(test.getVertices()[3].z() == fiveV.z());
+
+        QVector3D fiveN(0,0,1);
+        CHECK(test.getNormals()[3].x() == fiveN.x());
+        CHECK(test.getNormals()[3].y() == fiveN.y());
+        CHECK(test.getNormals()[3].z() == fiveN.z());
+
+        QVector2D fiveT(0.625000, 0.750000);
+        CHECK(test.getTextures()[3].x() == fiveT.x());
+        CHECK(test.getTextures()[3].y() == fiveT.y());
+    }
+
+    SUBCASE("detailedCheckCube4"){
+        QVector3D fiveV(-1,-1,1);
+        CHECK(test.getVertices()[4].x() == fiveV.x());
+        CHECK(test.getVertices()[4].y() == fiveV.y());
+        CHECK(test.getVertices()[4].z() == fiveV.z());
+
+        QVector3D fiveN(0,0,1);
+        CHECK(test.getNormals()[4].x() == fiveN.x());
+        CHECK(test.getNormals()[4].y() == fiveN.y());
+        CHECK(test.getNormals()[4].z() == fiveN.z());
+
+        QVector2D fiveT(0.375000, 1.000000);
+        CHECK(test.getTextures()[4].x() == fiveT.x());
+        CHECK(test.getTextures()[4].y() == fiveT.y());
+    }
 }
 
 TEST_CASE("checkCude"){
