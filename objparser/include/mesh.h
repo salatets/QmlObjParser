8c8643a15c94735527c3a4695065b8577036520a
Danila Kryukov <danila.kryukov.dev@yandex.ru>
2020-09-27 13:43:13 +0700

    fix mesh api
    Signed-off-by: Danila Kryukov <danila.kryukov.dev@yandex.ru>

--- objparser/include/mesh.h
+++ objparser/include/mesh.h
@@ -14,11 +14,11 @@ public:
     bool parseOBJ(const std::string& path);
 
     char getFormat() const {return format;}
-    QVector3D getCenter() const {return center;}
-    QVector3D getSize() const {return size;}
-    std::vector<QVector3D> getVertices() const {return vertices;}
-    std::vector<QVector2D> getTextures() const {return textures;}
-    std::vector<QVector3D> getNormals() const {return normals;}
+    const QVector3D& getCenter() const {return center;}
+    const QVector3D& getSize() const {return size;}
+    const std::vector<QVector3D>& getVertices() const {return vertices;}
+    const std::vector<QVector2D>& getTextures() const {return textures;}
+    const std::vector<QVector3D>& getNormals() const {return normals;}
 
 private:
     std::vector<QVector3D> vertices;
