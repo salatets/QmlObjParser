3758f228141cb36a0ad00d72b6479d191c80ffef
Danila Kryukov <danila.kryukov.dev@yandex.ru>
2020-12-16 00:50:48 +0700

    Init russian translation
    Signed-off-by: Danila Kryukov <danila.kryukov.dev@yandex.ru>

--- i18n/RGZ_ru.ts
+++ i18n/RGZ_ru.ts
@@ -5,51 +5,51 @@
     <name>main</name>
     <message>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
         <source>&amp;Open...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Открыть...</translation>
     </message>
     <message>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Выйти</translation>
     </message>
     <message>
         <source>&amp;Labs</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Работы</translation>
     </message>
     <message>
         <source>Lab 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Лаб 1</translation>
     </message>
     <message>
         <source>Lab 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Лаб 2</translation>
     </message>
     <message>
         <source>RGZ</source>
-        <translation type="unfinished"></translation>
+        <translation>РГЗ</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Помощь</translation>
     </message>
     <message>
         <source>Perspective</source>
-        <translation type="unfinished"></translation>
+        <translation>Персектива</translation>
     </message>
     <message>
         <source>light position</source>
-        <translation type="unfinished"></translation>
+        <translation>позиция освещения</translation>
     </message>
     <message>
         <source>Please choose a .obj file</source>
-        <translation type="unfinished"></translation>
+        <translation>Пожалуйста выберете .obj файл</translation>
     </message>
     <message>
         <source>help</source>
-        <translation type="unfinished"></translation>
+        <translation>Помощь</translation>
     </message>
     <message>
         <source>&lt;h1&gt;Controls&lt;/h1&gt;
@@ -64,7 +64,44 @@ In different modes may be additional control panel, dont shy to use it&lt;br&gt;
 &lt;li&gt; mesh sx sy sz tx ty tz name.obj&lt;br&gt;
 there are first coordinats for object scale in scene, second for translation
 </source>
-        <translation type="unfinished"></translation>
+        <translation type="vanished">&lt;h1&gt;Управление&lt;/h1&gt;
+&lt;p&gt;Удерживайте и передвигайте мышью чтобы вращать обьект&lt;br&gt;
+Используйте колёсико мыши для масштабирования&lt;br&gt;
+В различных режимах просмотра доступны дополнительные элементы управления,не стесняйтесь их использовать&lt;br&gt;
+&lt;h1&gt;Поддерживаемые форматы&lt;/h1&gt;
+&lt;p&gt; на данный момент поддерживаюся .obj and .scene форматы&lt;br&gt;
+&lt;h1&gt;формат сцены&lt;/h1&gt;
+&lt;p&gt;.scene файл содержит(разделитель - новая строка):
+&lt;ul&gt;
+&lt;li&gt; mesh sx sy sz tx ty tz name.obj&lt;br&gt;
+где превая координата - это размер,а вторая - положение
+</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;Controls&lt;/h1&gt;
+&lt;p&gt;Hold and move mouse to rotate object&lt;br&gt;
+Use mouse wheel for zoom object&lt;br&gt;
+In different modes may be additional control panel, dont shy to use it&lt;br&gt;
+&lt;h1&gt;Supported formats&lt;/h1&gt;
+&lt;p&gt; in this moment project support .obj and .scene formats&lt;br&gt;
+&lt;h1&gt;Scene format&lt;/h1&gt;
+&lt;p&gt;.scene files constain(separator - new line):
+&lt;ul&gt;
+&lt;li&gt; mesh sx sy sz tx ty tz name.obj&lt;br&gt;
+there are first coordinats for object scale in scene, second for translation
+</source>
+        <translation>&lt;h1&gt;Управление&lt;/h1&gt;
+&lt;p&gt;Удерживайте и передвигайте мышью чтобы вращать обьект&lt;br&gt;
+Используйте колёсико мыши для масштабирования&lt;br&gt;
+В различных режимах просмотра доступны дополнительные элементы управления,не стесняйтесь их использовать&lt;br&gt;
+&lt;h1&gt;Поддерживаемые форматы&lt;/h1&gt;
+&lt;p&gt; на данный момент поддерживаюся .obj and .scene форматы&lt;br&gt;
+&lt;h1&gt;формат сцены&lt;/h1&gt;
+&lt;p&gt;.scene файл содержит(разделитель - новая строка):
+&lt;ul&gt;
+&lt;li&gt; mesh sx sy sz tx ty tz name.obj&lt;br&gt;
+где превая координата - это размер,а вторая - положение
+</translation>
     </message>
 </context>
 </TS>
